a231d68677ee7e78d7976b655a6dc2bc8253ebba
kyrann9 <147066053+kyrann9@users.noreply.github.com>
2024-12-24 14:01:28 +0100

    Add files via upload

--- Model.c
+++ Model.c
@@ -4,10 +4,7 @@
 #include <stdlib.h>
 #define HEIGHT 80
 #define WIDTH 60
-
-typedef struct{
-    int score1,score2;
-}Score;
+#define MAX_SCORE 3
 
 Board * init_game(){
     Board * b=(Board *)malloc(sizeof(Board));
@@ -23,7 +20,8 @@ Board * init_game(){
         exit(EXIT_FAILURE);
     }
     b->p1->x=5;
-    b->p1->y=5;
+    b->p1->y=10;
+    b->p1->dirx=1,b->p1->diry=0;
     b->p1->tail=NULL;
     b->p2=(Player *)malloc(sizeof(Player));
     if(!b->p2){
@@ -32,62 +30,112 @@ Board * init_game(){
     }
     b->p2->x=10;
     b->p2->y=10;
+    b->p2->dirx=-1,b->p2->diry=0;
     b->p2->tail=NULL;
     return b;
 }
 
-void destroy_player(Player * p){
-    if(p){
-        destroy_player(p->tail);
-        free(p);
+Score * init_Score(){
+    Score * s=(Score *)malloc(sizeof(Score));
+    s->scoreP1=0; s->scoreP2=0;
+    return s;
+}
+void destroy_player(Tail * t){
+    if(t){
+        destroy_player(t->tail);
+        free(t);
     }
 }
-void destroy_game(Board * b){
-    destroy_player(b->p1);
-    destroy_player(b->p2);
+void destroy_game(Board * b,Score *s){
+    Tail * t=b->p1->tail;
+    destroy_player(b->p1->tail);
+    t=b->p2->tail;
+    free(b->p1);
+    destroy_player(b->p2->tail);
+    free(b->p2);
+    free(s);
     free(b);
 }
 
 int collision_check(Board * board, Player * player){
-    int nb_col=0; //there will always be a collision with their own heads, so we check for 2 collision
-    Player * parc=board->p1;
-    printf("%d %d player\n",player->x,player->y);
+    //Cette condition est très longue pck cette fonction ne sait pas quelle joueur elle check, à modifié peut-être ?
+    if(player->x<=0 || player->y<=0 || player->x>=WIDTH || player->y>=HEIGHT)return 1;
+    if(board->p1->x==player->x && board->p1->y==player->y && board->p2->x==player->x && board->p2->y==player->y)return 1;
+    Tail * parc=board->p1->tail;
     while(parc!=NULL){
-        printf("%d %d parc\n",parc->x,parc->y);
-        if(parc->x=player->x && parc->y==player->y){
-            nb_col++;
-            if(nb_col>1){printf("p1 collision\n");return 1;}
+        if(parc->x==player->x && parc->y==player->y){
+            return 1;
         }
         parc=parc->tail;
     }
-    parc=board->p2;
+    parc=board->p2->tail;
     while(parc!=NULL){
-        printf("%d %d parc\n",parc->x,parc->y);
-        if(parc->x=player->x && parc->y==player->y){
-            nb_col++;
-            if(nb_col>1){printf("p2 collision\n");return 1;}
+        if(parc->x==player->x && parc->y==player->y){
+            return 1;
         }
         parc=parc->tail;
     }
-    printf("Pas de collision %d\n", nb_col);
     return 0;
 }
-void add_tail(Player ** p,int x, int y){
-    Player * nhead=(Player *)malloc(sizeof(Player));
+
+void direction_left(Board* b, int nbPLayer){
+    if(nbPLayer){
+        b->p2->dirx=-1; b->p2->diry=0;
+        return;
+    }
+    b->p1->dirx=-1; b->p1->diry=0;
+}
+void direction_right(Board* b, int nbPLayer){
+    if(nbPLayer){
+        b->p2->dirx=1; b->p2->diry=0;
+        return;
+    }
+    b->p1->dirx=1; b->p1->diry=0;
+}
+void direction_down(Board* b, int nbPLayer){
+    if(nbPLayer){
+        b->p2->dirx=0; b->p2->diry=1;
+        return;
+    }
+    b->p1->dirx=0; b->p1->diry=1;
+}
+void direction_up(Board* b, int nbPLayer){
+    if(nbPLayer){
+        b->p2->dirx=0; b->p2->diry=-1;
+        return;
+    }
+    b->p1->dirx=0; b->p1->diry=-1;
+}
+void add_tail(Player ** p){
+    Tail * nhead=(Tail *)malloc(sizeof(Tail));
     if(!nhead){
-        perror("Error Player Allocation");
+        perror("Error Tail Allocation");
         exit(EXIT_FAILURE);
     }
-    nhead->x=x;nhead->y=y;
-    nhead->tail=*p;
-    *p=nhead;
+    nhead->x=(*p)->x;nhead->y=(*p)->y;
+    nhead->tail=(*p)->tail;
+    (*p)->tail=nhead;
+    (*p)->x+=(*p)->dirx,(*p)->y+=(*p)->diry;
 }
 
+void update_score(Board * game,Score * scr){
+    int tmp1,tmp2;
+    tmp1=collision_check(game,game->p1);
+    tmp2=collision_check(game,game->p2);
+    if(tmp1 && !tmp2)scr->scoreP2++;
+    if(tmp2 && !tmp1)scr->scoreP1++;
+}
 int main(){
     Board * b=init_game();
-    add_tail(&(b->p2),9,9);
-    add_tail(&(b->p1),10,10);
-    collision_check(b,b->p1);
-        collision_check(b,b->p2);
-    destroy_game(b);
+    Score * s=init_Score();
+    
+    for(int i=0;i<5;i++){
+        if(i==4)direction_up(b,0);
+        add_tail(&(b->p2));
+        add_tail(&(b->p1));
+
+        update_score(b,s);
+        printf("score P1=%d, socr P2=%d\n",s->scoreP1,s->scoreP2);
+    }
+    destroy_game(b,s);
 }
--- Model.h
+++ Model.h
@@ -1,22 +1,38 @@
 #ifndef MODEL_H
 #define MODEL_H
 
-struct _player{
+struct _tail {
+    struct _tail * tail;
     int x,y;
-    struct _player * tail;
+};
+typedef struct _tail Tail;
+
+struct _player{
+    Tail * tail;
+    int x,y;
+    short dirx,diry;
 };
 typedef struct _player Player;
 
 typedef struct {
-    int width,height;
     Player * p1;
     Player * p2;
+    int width,height;
 }Board;
 
+typedef struct{
+    short scoreP1,scoreP2;
+}Score;
+
+
 Board * init_game();
-void destroy_player(Player * p);
-void destroy_game(Board * b);
+void destroy_player(Tail * p);
+void destroy_game(Board * b,Score *s);
 int collision_check(Board * board, Player * player);
-void add_tail(Player ** p,int x, int y);
+void add_tail(Player ** p);
+void direction_up(Board* b, int nbPLayer);
+void direction_right(Board* b, int nbPLayer);
+void direction_down(Board* b, int nbPLayer);
+void direction_left(Board* b, int nbPLayer);
 
 #endif
